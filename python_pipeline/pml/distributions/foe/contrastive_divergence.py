eff2c942b3181a87de810650d9005273be0d5505
Yosua Muliawan <yosua.muliawan@shopee.com>
2021-03-20 19:25:21 +0800

    updated CD pseudocode

--- python_pipeline/pml/distributions/foe/contrastive_divergence.py
+++ python_pipeline/pml/distributions/foe/contrastive_divergence.py
@@ -1,23 +1,27 @@
-
-
-iterations = 10000;
-
+iterations = 3000
+learning_rate = 0.01
+clique_size = [5, 5]
+no_of_filters = 24
+mini_batch_size = 200 
 # We want to minimize the KL divergence between
-# Distribution of samples from the dataset, and distribution represented by our model
+# Distribution of samples from the dataset, and distribution represented by our modeld
 
+data = load_data("../../")
 
 for iteration in range(iterations):
 
-
+    # Get minibatch of 200 images from dataset
+    mini_batch_data = getMiniBatchData(data, mini_batch_size)
 
     # Gibbs sampling
-
-
+    sampled_data = HybridMonteCarlo(mini_batch_data)
 
     # Derivative wrt equilibrium dist over visible variables (our model)
-    pos =   
+    deriv_wrt_model = derive(model)
 
     # Derivative wrt sampled data 
-    neg = 
+    deriv_wrt_sampled_data = derive(sampled_data)
 
+    # Update weights according to KL divergence
     weights = weights + learning_rate * (pos + neg)
+
